4d67a71a670c901d62738073994368b731787088
helenjsdev <helen-balabanova@mail.ru>
2022-06-05 00:13:49 +0200

    feat: complete rename.js subtask

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,30 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const rename = async () => {
-    // Write your code here 
+    let file = path.join(__dirname, 'files', 'wrongFilename.txt');
+    let fileRenamed = path.join(__dirname, 'files', 'properFilename.md');
+    let err = 'FS operation failed';
+    fs.access(file, (error) => {
+        if (error) {
+            throw new Error(err);
+        } else {
+            fs.access(fileRenamed, (error) => {
+                if (error) {
+                    fs.rename(file, fileRenamed, () => {
+                        console.log('File was successfully renamed!');
+                    })
+                } else {
+                    throw new Error(err);
+                }
+            })
+        }
+    })
 };
+
+rename();
