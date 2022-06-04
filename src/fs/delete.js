7eddc90d2063bb0fc97c516acca4e0145c2779d1
helenjsdev <helen-balabanova@mail.ru>
2022-06-05 00:25:38 +0200

    feat: complete delete.js subtask

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+    let file = path.join(__dirname, 'files', 'fileToRemove.txt');
+    let err = 'FS operation failed';
+    fs.access(file, (error) => {
+        if (error) {
+            throw new Error(err);
+        } else {
+            fs.unlink(file, () => {
+                console.log('File was successfully removed!');
+            })
+        }
+    })
 };
+
+remove();
