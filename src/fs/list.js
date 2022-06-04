ee32b2140700750e6599119def91cbb5c83ff3d4
helenjsdev <helen-balabanova@mail.ru>
2022-06-05 00:34:41 +0200

    feat: complete list.js subtask

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const list = async () => {
-    // Write your code here 
+    let folder = path.join(__dirname, 'files');
+    let err = 'FS operation failed';
+    fs.access(folder, (error) => {
+        if (error) {
+            throw new Error(err);
+        } else {
+            fs.readdir(folder, (error, files) => {
+                files.forEach(file => {
+                    console.log(file);
+                })
+            })
+        }
+    })
 };
+
+list();
