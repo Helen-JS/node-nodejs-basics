3bb1026157578e889717cabc88ab50eaced609e3
helenjsdev <helen-balabanova@mail.ru>
2022-06-04 22:05:07 +0200

    feat: complete create.js subtask

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,27 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+    let file = path.join(__dirname, 'files', 'fresh.txt')
+    // if (fs.exists(file)) {
+    //     throw 'FS operation failed';
+    // }
+    let err = 'FS operation failed';
+    fs.access(file, (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    })
+
+    fs.appendFile(file, 'I am fresh and young', () => {
+        console.log('File was successfully created!');
+    })
 };
+
+create();
